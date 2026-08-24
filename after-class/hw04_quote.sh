a904481039ae11596cf4c37490271edcd3aa60c4
fernandes8 <fernandes8@example.com>
2026-08-24 06:09:44 -0700

    homework:add hw04

---
+++ after-class/hw04_quote.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "--- single quote ---"
+echo 'price is $100! \n test'
+
+echo "--- double quote ---"
+echo "price is $100! \n test"
+
+echo "--- dollar‑single quote \$' ' ---"
+echo $'price is $100!\nsecond line test'
